998d33097eb36753650d588d8d3d9c0c9381c1ed
Neghouche <neghouca@emea.roche.com>
2017-11-10 16:26:03 +0100

    backup

--- src/main/java/roche/fghsp/controller/SolutionController.java
+++ src/main/java/roche/fghsp/controller/SolutionController.java
@@ -1,16 +1,31 @@
 package roche.fghsp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import roche.fghsp.model.Solution;
+import roche.fghsp.repository.SolutionRepository;
 
 @Controller
 @RequestMapping("/solution")
 public class SolutionController {
 
+    @Autowired
+    private SolutionRepository solRep; 
 	
 	
-	
-	
+    @GetMapping({"/{id}","/details/{id}"})
+    public String index(Model model, @PathVariable("id") String id) {
+    	
+    	Solution solution = solRep.findOne(id);
+        model.addAttribute("solution", solution);
+
+        return "solutions/details";
+    }
 	
 	
 	
